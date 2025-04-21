91cefc2fa77ce49fc887066ea952ea3aa088b7ce
Chathumi21 <chathumiisurika0221@gmail.com>
2025-04-21 17:39:05 +0530

    Updated Delivery Frontend

--- Server/Delivery_Service/src/main/java/com/DeliveryOrder/DeliveryOrder/controller/DeliveryController.java
+++ Server/Delivery_Service/src/main/java/com/DeliveryOrder/DeliveryOrder/controller/DeliveryController.java
@@ -1,13 +1,20 @@
 package com.DeliveryOrder.DeliveryOrder.controller;
 
+import com.DeliveryOrder.DeliveryOrder.model.Delivery;
 import com.DeliveryOrder.DeliveryOrder.model.LocationDTO;
 import com.DeliveryOrder.DeliveryOrder.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//
+
 @RestController
 @RequestMapping("/api/v1/delivery")
+@CrossOrigin(origins = "http://localhost:5173",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+                RequestMethod.DELETE, RequestMethod.OPTIONS},
+        allowCredentials = "true",
+        maxAge = 3600)
 @RequiredArgsConstructor
 public class DeliveryController {
 
@@ -16,7 +23,7 @@ public class DeliveryController {
     @PostMapping("/update-location")
     public ResponseEntity<String> updateDriverLocation(@RequestBody LocationDTO dto) {
         deliveryService.updateLocation(dto);
-        return ResponseEntity.ok("Location updated!");
+        return ResponseEntity.ok("Location updated successfully");
     }
 
     @PostMapping("/create")
@@ -24,12 +31,18 @@ public class DeliveryController {
                                                  @RequestParam double latitude,
                                                  @RequestParam double longitude) {
         deliveryService.createDelivery(orderId, latitude, longitude);
-        return ResponseEntity.ok("Delivery created!");
+        return ResponseEntity.ok("Delivery created and assigned to nearest available driver");
     }
 
     @PostMapping("/mark-delivered/{driverId}")
     public ResponseEntity<String> markAsDelivered(@PathVariable String driverId) {
         deliveryService.markAsDelivered(driverId);
-        return ResponseEntity.ok("Delivery marked as delivered");
+        return ResponseEntity.ok("Delivery marked as delivered and driver is now available");
+    }
+
+    @GetMapping("/by-driver/{driverId}")
+    public ResponseEntity<Delivery> getDeliveryByDriver(@PathVariable String driverId) {
+        Delivery delivery = deliveryService.getDeliveryByDriver(driverId);
+        return ResponseEntity.ok(delivery);
     }
 }
