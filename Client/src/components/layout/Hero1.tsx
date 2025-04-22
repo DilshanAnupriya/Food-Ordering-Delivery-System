5b6caffdd7c3b0d580e4b6b76980a8d688e349a0
Dilshan <dilshananupriya@icloud.com>
2025-04-22 18:03:58 +0530

    2

---
+++ Client/src/components/layout/Hero1.tsx
@@ -0,0 +1,53 @@
+
+import {textVariant} from "../../util/motion.ts";
+import { motion } from 'framer-motion';
+
+interface restaurantProps {
+    name: string;
+    img: string;
+}
+
+const RestaurantCard:React.FC<restaurantProps>  = ({name,img}) => {
+    return (
+        <div className="pt-8 max-w-6xl mx-auto">
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+
+                    <div className="flex flex-col rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300">
+                        <div className=" h-48  flex items-center justify-center ">
+                            <img
+                                src={img}
+                                alt=""
+                                className="max-h-full max-w-full object-contain"
+                            />
+                        </div>
+                        <div className="p-3 mt-[-20px] text-center bg-amber-500 text-white font-medium">
+                            <h2>{name}</h2>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    );
+};
+
+
+
+
+
+const Hero1 = () => {
+    return (
+       <>
+           <motion.div variants={textVariant(0.3)} >
+               <h2 className="text-black font-black md:text-[30px] sm:text-[50px] xs:text-[40px] text-[18px] pt-10 pl-2">Popular Restaurants</h2>
+           </motion.div>
+               <RestaurantCard
+               name="Mac Donoald's"
+               img='src/assets/mac.png'/>
+
+
+       </>
+    );
+};
+
+export default Hero1;
