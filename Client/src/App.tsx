007b51d5d1e8689516da6dc00a6bb39c1a97f58a
JinaNavo <it22070012@my.sliit.lk>
2025-04-23 20:12:49 +0530

    updated

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,10 +4,8 @@ import ContactUs from './pages/ContactUs/ContactUs';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Checkout from './pages/Payment/Checkout';
-import OrderList from './pages/orders/OrderList';
-import OrderDetail from './pages/orders/OrderDetail';
-import OrderForm from './pages/orders/OrderForm';
-import OrderTracking from './pages/orders/OrderTracking';
+
+
 
 function App() {
     return (
@@ -17,11 +15,8 @@ function App() {
                 <Route path="/contact" element={<ContactUs />} />
                 <Route path="/" element={<Home/>} />
                 <Route path="/checkout" Component={Checkout} />
-                <Route path="/orders" element={<OrderList />} />
-                <Route path="/orders/new" element={<OrderForm />} />
-                <Route path="/orders/:orderId" element={<OrderDetail />} />
-                <Route path="/orders/:orderId/edit" element={<OrderForm />} />
-                <Route path="/track" element={<OrderTracking />} />
+                
+                
             </Routes>
             <Footer />
         </Router>
