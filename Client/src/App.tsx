aa8ba25299430e4b92bae8b7819f15b3f3f6c636
JinaNavo <it22070012@my.sliit.lk>
2025-04-27 11:03:08 +0530

    updated

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,6 +3,10 @@ import Home from './pages/Home';
 import Restaurants from './pages/Restaurant/Restaurants';
 import Restaurant from "./pages/Restaurant/Restaurant.tsx";
 import RestaurantCreate from "./pages/Restaurant/RestaurantCreate.tsx";
+import OrderDetail from './pages/orders/OrderDetail';
+import OrderForm from './pages/orders/OrderForm';
+import OrderTracking from './pages/orders/OrderTracking';
+import OrderList from './pages/orders/OrderList';
 
 function App() {
     return (
@@ -12,6 +16,12 @@ function App() {
                 <Route path="/restaurants" element={<Restaurants />} />
                 <Route path="/restaurant/:id" element={<Restaurant />} />
                 <Route path="/restaurant/create" element={<RestaurantCreate />} />
+                <Route path="/orders" element={<OrderList />} />
+                <Route path="/orders/new" element={<OrderForm />} />
+                <Route path="/orders/:orderId" element={<OrderDetail />} />
+                <Route path="/orders/:orderId/edit" element={<OrderForm />} />
+                <Route path="/track" element={<OrderTracking />} />
+
             </Routes>
         </Router>
     );
